f4eb97e40048623afb870486a93287445fdebe1f
antonnifo <antonnifo@live.com>
2019-07-31 23:21:08 +0300

    structure credit card

---
+++ credit_card.py
@@ -0,0 +1,44 @@
+class CreditCard:
+    "A consumer credit  card imitation."
+     
+    def __init__(self,customer,bank,acnt,limit):
+         self._customer = customer
+         self._bank     = bank
+         self._account   = acnt
+         self._limit     = limit
+         self._balance   = 0
+
+    def get_customer(self):
+
+        return self._customer
+
+    def get_bank(self):
+
+        return self._bank
+
+    def get_account(self):
+
+        return self._account
+
+    def get_limit(self):
+
+        return self._limit
+
+    def get_balance(self):
+
+        return self._balance
+
+    def charge(self,price):
+        '''Charge given price to the card, assuming sufficient credit limit.
+        Return True if charge was processed; False if charge was denied.
+        '''
+
+        if price + self._balance > self._limit:
+            return False
+        else:
+            self._balance += price
+            return True
+
+    def make_payment(self,amount):
+        """Process customer payment that reduces balance."""
+        self._balance -= amount        
